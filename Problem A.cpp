60ca7ced639d141908d4e864599ac167d6e43909
Ivan <ivan013@binus.ac.id>
2020-11-02 17:00:52 +0700

    Problem A C++

---
+++ Problem A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if (a>b) cout<<a;
+    else if (a<b) cout<<b;
+    else if (a==b) cout<<a;
+    return 0;
+}
